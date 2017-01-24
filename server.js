cc7f54c8eadb8cd408069daaf31885c45a248b08
racifuina <racifuina@me.com>
2017-01-24 12:44:07 -0600

    make it better

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync('index.html');
+var HTTP_PORT = process.env.PORT || 8080;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
+}).listen(HTTP_PORT);
