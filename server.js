e574bffeba48d5347f0acc14b321126fcc5f362b
Rigoberto Acifuina <racifuina@me.com>
2022-03-19 10:10:55 -0600

    cambios

--- server.js
+++ server.js
@@ -9,12 +9,14 @@ January, 2017
 Static content server for the privacy policy.
 */
 
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
+var http = require("http");
+var fs = require("fs");
+var index = fs.readFileSync("index.html");
 var HTTP_PORT = process.env.PORT || 8080;
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(index);
-}).listen(HTTP_PORT);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(index);
+  })
+  .listen(HTTP_PORT);
